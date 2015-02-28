bbea3a3305f83b665ed8cc4d0d5418ba290f93af
alweky <alweky@yahoo.ca>
2015-02-27 19:03:27 -0500

    Most up to  date

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,5 +1,11 @@
 var ie = 0, ff = 0, ch = 0, sa = 0;
 
+/* $('.slider').glide({
+	autoplay: 5000,
+	arrows: 'body',
+	navigation: 'body'
+}); */
+
 /* $(document).ready(function () {
     //initialize swiper when document ready  
     var mySwiper = new Swiper ('.swiper-container', {
@@ -12,8 +18,34 @@ var ie = 0, ff = 0, ch = 0, sa = 0;
     })        
 }); 
  */
+ $(document).ready(function(){
+	$("#my_slider").muslider({
+		"responsive": true,
+		"animation_start": "auto",
+		"animation_interval": 3000
+	});
+	$("#my_sliderAndroidHealth").muslider({
+		"responsive": true,
+		"animation_start": "auto",
+		"animation_interval": 3000
+	});
+	$("#my_sliderAdvancedSpaceWars").muslider({
+		"responsive": true,
+		"animation_start": "auto",
+		"animation_interval": 3000
+	});
+	$("#my_sliderJumper").muslider({
+		"responsive": true,
+		"animation_start": "auto",
+		"animation_interval": 3000
+	});
+	$("#my_sliderSolarSystem").muslider({
+		"responsive": true,
+	});
+});
+ 
 $(function () {
-    $("#sliderHome").excoloSlider({
+ /*    $("#sliderHome").excoloSlider({
         mouseNav: false,
         interval: 2000,
         playReverse: true,
@@ -25,31 +57,20 @@ $(function () {
         interval: 2000,
         playReverse: true,
 		autoSize: true,
-		height: 450
+		height: 650
     });
-	$("#my_sliderAdvancedSpaceWars").excoloSlider({
+	$("#sliderAdvancedSpaceWarsApp").excoloSlider({
         mouseNav: false,
         interval: 2000,
         playReverse: true,
 		autoSize: true,
-		height: 450
-    });
-	$("#my_sliderSolarSystem").excoloSlider({
-        mouseNav: false,
-        interval: 2000,
-        playReverse: true,
-		autoSize: true,
-		height: 450
-    });
-	$("#my_sliderJumper").excoloSlider({
-        mouseNav: false,
-        interval: 2000,
-        playReverse: true,
-		autoSize: true,
-		height: 450
-    });
+		height: 650
+    }); */
+	
 });
+
+
  
 function init(){	
-
+	//$('.slider').glide();
 }
